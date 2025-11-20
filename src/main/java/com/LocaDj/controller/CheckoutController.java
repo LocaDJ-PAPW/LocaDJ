3000c277d85b320e5a32e17d3dea31a2d14f2943
vanilson <vanilson.liins@gmail.com>
2025-11-19 21:14:15 -0300

    bugfix do pagamento pendente

--- src/main/java/com/LocaDj/controller/CheckoutController.java
+++ src/main/java/com/LocaDj/controller/CheckoutController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
@@ -17,8 +18,9 @@ import java.util.List;
 @Slf4j
 public class CheckoutController {
 
-    @GetMapping()
-    public String showCheckoutPage() {
+    @GetMapping("/{id}")
+    public String showCheckoutPage(@PathVariable long id, Model model) {
+        model.addAttribute("ReservationId", id);
         return "checkout/checkout";
     }
 
